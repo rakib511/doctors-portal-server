33192b53771253db1e33b1de9900c0d4ed78d380
rakib511 <rakib660299@gmail.com>
2021-11-08 01:40:12 -0800

    last commit

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ async function run() {
 
         app.get('/appointments', async (req, res) => {
             const email = req.query.email;
-            const date = new Date(req.query.date).toLocaleDateString();
+            const date = req.query.date;
 
             const query = { email: email, date: date }
 
